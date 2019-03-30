500780fabd1c17aef933e7865160d5ffda658330
Vincent Poesmann <vincent.poesmann@gmail.com>
2019-03-30 11:08:28 +0100

    Add backend support

--- app/redux/actions/command.js
+++ app/redux/actions/command.js
@@ -1,73 +1,18 @@
+import { backendApiUrl } from '../../utils/utils'
+
 export const LOAD_PRODUCTS = 'LOAD_PRODUCTS'
 
-export const loadProducts = () => ({
-  type: LOAD_PRODUCTS,
-  products: {
-    alcohol: [{
-      id: 'a01',
-      name: 'Vodka pure 1L',
-      image: 'img',
-      description: 'Un grand bol de vodka pure',
-      price: 15.5,
-    }],
-    soft: [{
-      id: 's01',
-      name: 'Ice tea 33cl',
-      image: 'img',
-      description: 'Lipton Ice tea en canette 33cl',
-      price: 2,
-    },
-    {
-      id: 's02',
-      name: 'Cocal-cola 33cl',
-      image: 'img',
-      description: 'Coca-cola en canette 33cl',
-      price: 2,
-    },
-    {
-      id: 's03',
-      name: 'Oasis tropical 33cl',
-      image: 'img',
-      description: 'Oasis goût tropical en canette 33cl',
-      price: 2,
-    },
-    {
-      id: 's04',
-      name: 'Cristalline eau plate 50cl',
-      image: 'img',
-      description: 'Bouteille d\'eau plate cristalline 50cl',
-      price: 1.5,
-    },
-    {
-      id: 's05',
-      name: 'Dr.Pepper 33cl',
-      image: 'img',
-      description: 'Dr.Pepper en canette 33cl',
-      price: 2.5,
-    }],
-    hot: [{
-      id: 'h01',
-      name: 'Cappuccino',
-      image: 'img',
-      description: 'Cappuccino 20cl',
-      price: 1,
-    }],
-    meal: [{
-      id: 'm01',
-      name: 'Panini montagnard',
-      image: 'img',
-      description: 'Panini raclette lardon pomme de terre',
-      price: 5.5,
-    }],
-    snack: [{
-      id: 'sn01',
-      name: 'Planche de charcuterie 100g',
-      image: 'img',
-      description: 'Assortiment de saucisson et autres trucs gras de 100g',
-      price: 5,
-    }],
-  }
-})
+export const loadProducts = () => dispatch => {
+
+  fetch(`${backendApiUrl}/products`)
+    .then(response => response.json())
+    .then(data => {
+      dispatch({
+        type: LOAD_PRODUCTS,
+        products: data,
+      })
+    })
+}
 
 export const SELECT_PRODUCT = 'SELECT_PRODUCT'
 
--- app/utils/utils.js
+++ app/utils/utils.js
@@ -1,1 +1,3 @@
 export const makeReducer = (initState, actions) => (state = initState, action) => actions[action.type] ? actions[action.type](state, action) : state
+
+export const backendApiUrl = 'http://localhost:8080/api'
